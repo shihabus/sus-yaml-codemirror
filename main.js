2c3bbb53babce18addaf0a5e542511eaa7c96fef
shihabus <shihabuudheen@gmail.com>
2022-02-24 15:48:04 +0530

    trying ace

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
-import { javascript } from '@codemirror/lang-javascript';
-import { yaml } from '@codemirror/legacy-modes/mode/yaml';
-import { StreamLanguage } from '@codemirror/stream-parser';
+// import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
+// import { javascript } from '@codemirror/lang-javascript';
+// import { yaml } from '@codemirror/legacy-modes/mode/yaml';
+// import { StreamLanguage } from '@codemirror/stream-parser';
 
-// mode/yaml
+// // mode/yaml
 
-let view = new EditorView({
-  state: EditorState.create({
-    extensions: [basicSetup, StreamLanguage.define(yaml)],
-  }),
-  parent: document.body,
+// let view = new EditorView({
+//   state: EditorState.create({
+//     extensions: [basicSetup, StreamLanguage.define(yaml)],
+//   }),
+//   parent: document.body,
+// });
+
+ace.require('ace/ext/language_tools');
+
+var editor = ace.edit('editor');
+// editor.setTheme('ace/theme/monokai');
+editor.session.setMode('ace/mode/yaml');
+editor.setOptions({
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: true,
+});
+
+editor.session.on('change', function (delta) {
+  // delta.start, delta.end, delta.lines, delta.action
+  const doc = editor.getValue();
 });
